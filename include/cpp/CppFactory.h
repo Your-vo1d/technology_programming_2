2a190e19144f27b80210967aae0f2aa02d939e06
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-06-07 16:27:07 +0700

    feat: move cpp code to folder cpp

---
+++ include/cpp/CppFactory.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "IFactory.h"
+
+class CppFactory : public IFactory
+{
+public:
+    std::shared_ptr<Unit> createClass(const std::string &name, Unit::Flags classFlags = 0, const std::string &baseClass = "") const override;
+
+    std::shared_ptr<Unit> createMethod(const std::string &name, const std::string &returnType, Unit::Flags flags) const override;
+
+    std::shared_ptr<Unit> createPrintOperator(const std::string &text) const override;
+};
